2e9ec1f97cccea0e75e3ba5c15145396ada21128
Pablo Martín <goinnn@gmail.com>
2012-03-20 18:23:11 +0100

    The urls should can contains ":" and "_"

--- form_designer/urls.py
+++ form_designer/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls.defaults import patterns, url
 
+NAME_RE = '[:\-_\w]+'
+
 urlpatterns = patterns('',
-    url(r'^(?P<object_name>[-\w]+)/$', 'form_designer.views.detail', name='form_designer_detail'),
-    url(r'^h/(?P<public_hash>[-\w]+)/$', 'form_designer.views.detail_by_hash', name='form_designer_detail_by_hash'),
-    url(r'^edit/(?P<object_name>[-\w]+)/$', 'form_designer.views.edit', name='form_designer_edit'),
-    url(r'^edit/h/(?P<public_hash>[-\w]+)/$', 'form_designer.views.edit_by_hash', name='form_designer_edit_by_hash'),
-    url(r'^save/(?P<object_name>[-\w]+)/$', 'form_designer.views.save', name='form_designer_save'),
+    url(r'^(?P<object_name>%s)/$' % NAME_RE, 'form_designer.views.detail', name='form_designer_detail'),
+    url(r'^h/(?P<public_hash>%s)/$' % NAME_RE, 'form_designer.views.detail_by_hash', name='form_designer_detail_by_hash'),
+    url(r'^edit/(?P<object_name>%s)/$' % NAME_RE, 'form_designer.views.edit', name='form_designer_edit'),
+    url(r'^edit/h/(?P<public_hash>%s)/$' % NAME_RE, 'form_designer.views.edit_by_hash', name='form_designer_edit_by_hash'),
+    url(r'^save/(?P<object_name>%s)/$' % NAME_RE, 'form_designer.views.save', name='form_designer_save'),
 )
